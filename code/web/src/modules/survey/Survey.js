9de4c6f4f1295d6c9e93e3c6a1c60103dc89f1b7
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-29 14:27:12 -0600

    Add SurveyModal style

---
+++ code/web/src/modules/survey/Survey.js
@@ -0,0 +1,8 @@
+// Imports
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+// UI Imports
+import { Grid, GridCell } from '../../ui/grid'
+import { H3 } from "../../ui/typography";
