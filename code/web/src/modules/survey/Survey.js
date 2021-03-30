f6e61354da96b8e6c38e54d73c038130fc047beb
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-29 20:04:10 -0600

    Change style for Card component

--- code/web/src/modules/survey/Survey.js
+++ code/web/src/modules/survey/Survey.js
@@ -7,9 +7,13 @@ import crateRoutes from "../../setup/routes/crate";
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
 import { H3 } from "../../ui/typography";
-import  SurveyModal from '../../ui/surveyModal'
-import  Button  from '../../ui/button/Button'
+import SurveyModal from '../../ui/surveyModal/SurveyModal'
+import Button  from '../../ui/button/Button'
 import Card from "../../ui/card/Card";
+import { white } from "../../ui/common/colors";
+
+//App Imports
+import { APP_URL } from "../../setup/config/env";
 
 class Survey extends PureComponent {
     constructor(props) {
@@ -18,40 +22,43 @@ class Survey extends PureComponent {
            isLoading: false,
            page: 0,
            products: null,
-        //    results: [],
-        //    history: null 
+          }
         }
-    }
-
-    nextPage = () => {
-        this.setState(prevState => {
+        
+        nextPage = () => {
+          this.setState(prevState => {
             return {
               page: prevState.page + 1  
             }
-        })
-    }
-    
-    render() {
-        <SurveyModal>
-          {/* product display */}
-          <H3 font='secondary'>Style Survey</H3>
-          <Grid>
-            <GridCell>
-              <Card style={{ width: "30%", backgroundColor: white }}>
-                  <img src={`${ APP_URL }/images/crate.png`} alt={`a box`} style={{ width: '100%' }}/>
-              </Card>
-              {/* individual component */}
-              {/* add onClick on the image. src, alt, style, selected - false by default */}
-              {/* function that iterates over all products to display clickable images */}
-            </GridCell>
-          </Grid>
-          <Button theme="secondary" type="button" onClick={this.nextPage}>
-            Next Page
-          </Button>
-        </SurveyModal>;
+          })
+        }
+        
+        render() {
+          return (
+            <SurveyModal>
+            {/* product display */}
+            <H3 font='secondary'>Style Survey</H3>
+            <Grid style={{zIndex: 3}}>
+              <GridCell>
+                <Card style={{ width: "30%", backgroundColor: "#FFA", zIndex: 4 }}>
+                    <img src={`${ APP_URL }/images/crate.png`} alt={`a box`} style={{ width: '100%' }}/>
+                </Card>
+                {/* individual component */}
+                {/* add onClick on the image. src, alt, style, selected - false by default */}
+                {/* function that iterates over all products to display clickable images */}
+              </GridCell>
+            </Grid>
+            <Button theme="secondary" type="button" onClick={this.nextPage}>
+              Next Page
+            </Button>
+          </SurveyModal>
+          )
     }
     // this.props.history.push(userRoutes.subscriptions.path)
-}
-
-export default Survey
-
+  }
+  
+  export default Survey
+  
+  
+  //    results: [],
+  //    history: null 
