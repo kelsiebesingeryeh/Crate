9f477a058ea375ccae04b5475373c13ffbab18a8
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-31 17:17:49 -0600

    add resolver function for getSurveyProducts query

--- code/api/src/modules/product/resolvers.js
+++ code/api/src/modules/product/resolvers.js
@@ -2,6 +2,33 @@
 import params from '../../config/params'
 import models from '../../setup/models'
 
+// Survey products
+export async function getSurveyProducts(parentValue, { type, gender }, { auth }) {
+  if(auth.user && auth.user.id > 0) {
+    const products = await models.Product.findAll({
+      where: {
+        type: type,
+        gender: gender
+      }
+    });
+    const groups = products.reduce((groups, product) => {
+      let category = product.category;
+      groups[category] = [...groups[category] || [], product];
+      return groups;
+    }, {});
+    const formatted_groups = Object.entries(groups).reduce((fmt, group) => {
+      fmt.push({
+        "category": group[0],
+        "products": group[1]
+      })
+      return fmt
+    }, []);
+    return formatted_groups
+  } else {
+    throw new Error('Please login to subscribe to this crate.')
+  }
+}
+
 // Get all products
 export async function getAll() {
   return await models.Product.findAll({ order: [['id', 'DESC']] })
