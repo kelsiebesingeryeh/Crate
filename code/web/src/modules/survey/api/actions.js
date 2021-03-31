e4ad0b66238cd97592edeb875816ca8e49716632
Drew Bradley <williamandrewb@gmail.com>
2021-03-31 16:17:33 -0600

    Add redux files for survey.

---
+++ code/web/src/modules/survey/api/actions.js
@@ -0,0 +1,59 @@
+// Imports
+import axios from 'axios'
+import { query, mutation } from 'gql-query-builder'
+
+// App Imports
+import { routeApi } from '../../../setup/routes'
+
+// Actions Types
+export const SURVEY_GET_PRODUCTS = 'SURVEY_GET_PRODUCTS'
+export const SURVEY_GET_PRODUCTS_FAIL = 'SURVEY_GET_PRODUCTS_FAIL'
+export const SURVEY_NEXT_PAGE = 'SURVEY_NEXT_PAGE'
+
+// Actions
+
+// Get list of products
+export function getProducts(isLoading = true) {
+  return dispatch => {
+    // dispatch({
+    //   type: SURVEY_GET_PRODUCTS,
+    //   error: null,
+    //   isLoading
+    // })
+
+    return axios.post(routeApi, query({
+      operation: 'products',
+      fields: ['id', 'user { name, email }', 'crate { id, name, description }', 'createdAt']
+    }))
+      .then(response => {
+        if (response.status === 200) {
+          dispatch({
+            type: SURVEY_GET_PRODUCTS,
+            isLoading: false,
+            page: 1,
+            products: response.data.data.products
+          })
+        } else {
+          console.error(response)
+        }
+      })
+      .catch(error => {
+        dispatch({
+          type: SURVEY_GET_PRODUCTS_FAIL,
+          error: 'Some error occurred. Please try again.',
+          isLoading: false
+        })
+      })
+  }
+}
+
+
+// next page
+export function nextPage(initialPage) {
+  return dispatch => {
+    dispatch({
+      type: SURVEY_NEXT_PAGE,
+      page: initialPage + 1
+    })
+  }
+}
---
+++ code/web/src/modules/survey/api/state.js
@@ -0,0 +1,34 @@
+// Imports
+
+// App Imports
+import {
+  SURVEY_GET_PRODUCTS,
+  SURVEY_NEXT_PAGE,
+} from './actions'
+
+// Initial State
+const surveyInitialState = {
+  isLoading: false,
+  page: 1,
+  products: {}
+}
+
+// State
+export const survey = (state = surveyInitialState, action) => {
+  switch (action.type) {
+    case SURVEY_GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.products
+      }
+
+    case SURVEY_NEXT_PAGE:
+      return {
+        ...state,
+        page: action.page,
+      }
+
+    default:
+      return state
+  }
+}
