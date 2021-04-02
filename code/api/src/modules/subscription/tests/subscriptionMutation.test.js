9e7440d33550e69a758598e8337a4e218e63efc0
Yesi Meza <yesi.meza10@gmail.com>
2021-04-02 17:31:23 -0600

    removed hard coded crate id

--- code/api/src/modules/subscription/tests/subscriptionMutation.test.js
+++ code/api/src/modules/subscription/tests/subscriptionMutation.test.js
@@ -1,21 +1,23 @@
 import request from 'supertest';
 import { server, init, userLogin } from '../../../../test/testHelper';
-
+import models from '../../../setup/models'
 
 describe('subscription mutations', () => {
   init()
 
   it('can create a subscription', async () => {
     let userToken = await userLogin()
+    const crate = await models.Crate.findOne()
+    const crateId = crate.dataValues.id
 
     let response = await request(server)
       .post("/")
       .set("Authorization", `Bearer ${userToken}`)
       .set('Accept', 'application/json')
       .send({
-        query: `mutation { subscriptionCreate(crateId: 7, style: "Edgy") { id } }`
+        query: `mutation { subscriptionCreate(crateId: ${crateId}, style: "Edgy") { id } }`
       })
-      console.log(response.body)
+ 
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty("data");
     expect(response.body.data).toHaveProperty("subscriptionCreate");
